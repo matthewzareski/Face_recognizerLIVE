49420960d461ed1500536869240789b698fe7e2f
matthewzareski <78160277+matthewzareski@users.noreply.github.com>
2021-10-16 12:58:39 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
-function setup(){
-    canvas=createCanvas(300,300);
+function setup() {
+    canvas = createCanvas(300, 300);
     canvas.center();
-}
-
-function draw(){
-}
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('',modelLoaded);
+  }
+  
+  function draw() {
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotResult);
+  }
+  
+  function gotResult(error, results) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(results);
+      document.getElementById("result_obj").innerHTML = results[0].label;
+      document.getElementById("result_acc").innerHTML = results[0].confidence.toFixed(3);
+  
+    }
+  }
+  
+  function modelLoaded() {
+    console.log('Model Loaded');
+  }
+  
+  
+  
+  
+  
